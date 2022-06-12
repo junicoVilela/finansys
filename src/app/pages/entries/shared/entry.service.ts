770b8c69c19779d2869ff2dbd364b5cebef889b8
Valdemir Vilela Junior <junico.vilela@gmail.com>
2022-06-12 02:52:00 -0300

    Melhorias no "EntryService" (reduzindo mais repetições)

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,7 @@ import {flatMap, Observable} from "rxjs";
 import { BaseResourceService } from "../../../shared/services/base-resource.service";
 import { CategoryService } from "../../categories/shared/category.service";
 import { Entry } from "./entry.model";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -16,23 +17,22 @@ export class EntryService extends BaseResourceService<Entry>{
     }
 
     override create(entry: Entry): Observable<Entry> {
-        return this.categoryService.getById(entry.categoryId!).pipe(
-            flatMap(category => {
-                entry.category = category;
-                return super.create(entry);
-            })
-        );
+        return this.setCategoryAndSendToServer(entry, super.create.bind(this));
     }
 
     //fazer dessa forma somente em caso de utilizar angular-in-memory
     override update(entry: Entry): Observable<Entry> {
+        return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+    }
+
+    private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<any> {
         return this.categoryService.getById(entry.categoryId!).pipe(
             flatMap(category => {
                 entry.category = category;
-
-                return super.update(entry);
-            })
-        )
+                return sendFn(entry);
+            }),
+            catchError(this.handleError)
+        );
     }
 
 }
