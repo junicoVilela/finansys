6ac5567e7cde82995293122c6f81667aa795a637
Valdemir Vilela Junior <valdemir@softon.com.br>
2022-07-06 23:36:05 -0300

    ajustes

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Category } from "../shared/category.model";
 import { CategoryService } from "../shared/category.service";
--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Entry } from "../shared/entry.model";
 import { EntryService } from "../shared/entry.service";
