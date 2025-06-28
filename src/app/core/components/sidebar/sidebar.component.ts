109114a4ea89ad70a795355b72c1419112e0bda8
junico vilela <junico.vilela@gmail.com>
2025-06-27 21:42:44 -0300

    alinhando header deixando melhor visualmente

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   standalone: true,
-  imports: []
+  imports: [CommonModule, RouterModule]
 })
 export class SidebarComponent {
+  currentUser$: Observable<User | null>;
 
+  constructor(
+    private authService: AuthService
+  ) {
+    this.currentUser$ = this.authService.currentUser$;
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+  }
 }
